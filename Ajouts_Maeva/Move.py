2941f6a22b13a14153534b41a65f5e2d293d258f
Maeva G <maegrondin@enseirb-matmeca.fr>
2016-06-17 17:08:42 +0200

    test2

--- Ajouts_Maeva/Move.py
+++ Ajouts_Maeva/Move.py
@@ -4,7 +4,7 @@ from time import sleep
 def execute(src, serial):
 	for ligne in src:
 		serial.write(ligne)
-		sleep(0.02)
+		sleep(0.01)
 
 if len(sys.argv) != 3:
      print "Usage : python Move.py <source> <port>"
