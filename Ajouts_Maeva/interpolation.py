a1b06af315d7b5994f603651f3cee91ceeaa03c2
Maeva G <maegrondin@enseirb-matmeca.fr>
2016-06-21 15:43:48 +0200

    patch interpolation

--- Ajouts_Maeva/interpolation.py
+++ Ajouts_Maeva/interpolation.py
@@ -3,7 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
-
+def lissage(Lx, Ly, p):
+    Lxout=[]
+    Lyout=[]
+    Lxout= Lx[p: -p]
+    for i in range(p, len(Ly)-p):
+        val = 0
+        for k in range(2*p):
+            val += float(Ly[i-p+k])
+        Lyout.append(val/2/p)
+    return Lxout, Lyout
+    
 
 def interpolation(id_moteur, ligne_depart, ligne_arrivee, source):
 
@@ -11,9 +21,8 @@ def interpolation(id_moteur, ligne_depart, ligne_arrivee, source):
     src = open(source,"r")
 
     # Avancee jusqu'a ligne depart
-    for i in range(ligne_depart):
+    for i in range(1, ligne_depart):
         src.readline()
-
 
     # Stockage des valeurs du moteur
     valeurs_moteur = []
@@ -24,21 +33,19 @@ def interpolation(id_moteur, ligne_depart, ligne_arrivee, source):
     # Temps
     temps = np.arange(ligne_arrivee - ligne_depart + 1)
 
+    temps, valeurs_moteur = lissage(temps,valeurs_moteur,10)
+
     # Interpolation
-    f1 = interp1d(temps, valeurs_moteur) # lineaire
     f2 = interp1d(temps, valeurs_moteur, kind='cubic') #cubique
 
     # Affichage
-    xnew = np.linspace(ligne_depart, ligne_arrivee-1, ligne_arrivee*10)
-    plt.plot(temps, valeurs_moteur, 'o', xnew, f1(xnew), '.', xnew, f2(xnew), '-')
-    plt.show()
+    fig = plt.figure()
+    xnew = np.linspace(10, ligne_arrivee - ligne_depart-10, ligne_arrivee*10)
+    plt.plot(xnew, f2(xnew), '-')
+    name = "motor" + str(id_moteur) + ".png"
+    fig.savefig(name)
+    print "%s : DONE" % name
+    
 
     # Valeur de retour = interpolation cubique
     return f2
-
-
-
-
-print interpolation(3,1,500, "NAME_interpolation.txt")
-
-
